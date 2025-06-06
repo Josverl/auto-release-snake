6dd676cb4a78c84947785e47b681382bc1245733
Jos Verlinde <Jos_Verlinde@hotmail.com>
2025-06-06 10:45:09 +0200

    feat: snakeSay
    Signed-off-by: Jos Verlinde <Jos_Verlinde@hotmail.com>

--- auto_release_snake/cli.py
+++ auto_release_snake/cli.py
@@ -1,6 +1,6 @@
 
 
-def snake():
+def snake_old():
     """
     This function returns a string that represents a blue snake in ASCII art.
     """
@@ -14,6 +14,29 @@ def snake():
         " \\_/\\___/\\_/\n"
         "\033[0m" 
     )
+
+def snake():
+    snake_say("Hello, I am not a cow.")
+    
+
+def snake_say(message):
+    """
+    Prints a message as if the snake is saying it, similar to cowsay.
+    """
+    # Prepare the speech bubble
+    lines = message.split('\n')
+    max_length = max(len(line) for line in lines)
+    top = ' ' + '_' * (max_length + 2)
+    bottom = ' ' + '-' * (max_length + 2)
+    bubble = [top]
+    for line in lines:
+        bubble.append(f"< {line.ljust(max_length)} >")
+    bubble.append(bottom)
+    # Print the bubble and the snake
+    print('\n'.join(bubble))
+
+
 if __name__ == "__main__":
-    print(snake())
+
+    snake_say("Hello, I am not a cow.")
     
